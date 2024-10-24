057e7ba06de7827b2f5c36cfa3a75a28a501c84a
emanuel-phillipe <emanuelphillipe1706@gmail.com>
2024-10-24 11:11:59 -0300

    Added RequestLoading

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,14 @@ import Auth from "./components/Authentication/Auth";
 import { useCookies } from "react-cookie";
 import axios from "axios";
 import Admin from "./pages/Admin";
+import LoadingScreen from "./components/LoadingScreen";
 
 export function App() {
 
   const [quizState, dispatch] = useContext(QuizContext)
   const [cookies, setCookie, removeCookie] = useCookies(['userToken']);
   const [authDispatch, setAuthDispatch] = useState(false)
+  const [requesting, setRequesting] = useState(false)
 
   useEffect(() => {
     const auth = async () => {
@@ -22,12 +24,14 @@ export function App() {
         if(!cookies.userToken) {          
           dispatch({type: "USER_AUTH"})
         }else{
+          setRequesting(true)
           const verification = await axios.post(import.meta.env.VITE_API+"/auth/validate", {"token": cookies.userToken}).catch((err) => {
             removeCookie("userToken")
             dispatch({type: "USER_AUTH"})
             return false
           })
 
+          setRequesting(false)
           dispatch({type: "HOME_PAGE"})
         }
         
@@ -40,12 +44,13 @@ export function App() {
 
   return (
     <div className="">
+      {requesting && <LoadingScreen />}
       {quizState.gameStage === "Start" && <WelcomePage />}
       {quizState.gameStage === "Playing" && <Question />}
       {quizState.gameStage === "Creation" && <QuestionCreation />}
       {quizState.gameStage === "End" && <GameEnd />}
       {quizState.gameStage === "Incorrects" && <IncorrectQuestions />}
-      {quizState.gameStage === "Auth" && <Auth />}
+      {quizState.gameStage === "Auth" && !requesting && <Auth />}
       {quizState.gameStage === "Admin" && <Admin />}
     </div>
   )
